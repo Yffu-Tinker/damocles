4dc40267d9451c38db94afad1a0140caaddf097b
tinker <whoszus@yahoo.com>
2018-05-03 13:14:29 +0800

    提交微信模块改动

--- wechat/src/main/java/cc/tinker/controller/WechatController.java
+++ wechat/src/main/java/cc/tinker/controller/WechatController.java
@@ -3,6 +3,7 @@ package cc.tinker.controller;
 import cc.tinker.service.HandleRequestService;
 import cc.tinker.util.MessageUtil;
 import cc.tinker.wechat.Validate;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,23 +19,25 @@ import java.util.Map;
 @RestController
 public class WechatController {
 
-	@Autowired
-	private HandleRequestService requestService;
+    @Autowired
+    private HandleRequestService requestService;
 
-	@RequestMapping(value = "/wechat", method = RequestMethod.GET)
-	public String validate(String signature, String timestamp, String nonce, String echostr) {
-		String success = "";
-		if (Validate.validateSignature(signature, timestamp, nonce)) {
-			success = echostr;
-		}
-		return success;
-	}
+    @RequestMapping(value = "/wechat", method = RequestMethod.GET)
+    public String validate(String signature, String timestamp, String nonce, String echostr) {
+        String success = "";
+        if (Strings.isBlank(signature) || Strings.isBlank(timestamp) || Strings.isBlank(nonce)) {
+            return success;
+        }
+        if (Validate.validateSignature(signature, timestamp, nonce)) {
+            success = echostr;
+        }
+        return success;
+    }
 
-	@RequestMapping(value = "/wechat", method = RequestMethod.POST)
-	public String service(HttpServletRequest request) {
-		Map<String, String> map = MessageUtil.parseXml(request);
-		String response = requestService.handleRequest(map);
-		return response;
-	}
+    @RequestMapping(value = "/wechat", method = RequestMethod.POST)
+    public String service(HttpServletRequest request) {
+        Map<String, String> map = MessageUtil.parseXml(request);
+        return requestService.handleRequest(map);
+    }
 
 }
--- wechat/src/main/java/cc/tinker/service/HandleRequestService.java
+++ wechat/src/main/java/cc/tinker/service/HandleRequestService.java
@@ -24,13 +24,6 @@ import java.util.Map;
 @Service
 public class HandleRequestService {
 
-    @Autowired
-    private StudentService studentService;
-    @Autowired
-    private RelationService relationService;
-    @Autowired
-    private SignService signService;
-
     public String handleRequest(Map<String, String> map) {
         String response = "success";
         String msgType = map.get("MsgType");
@@ -66,26 +59,11 @@ public class HandleRequestService {
         String response = "success";
         String eventType = map.get("Event");
         String openId = map.get("FromUserName");
-        Student student = studentService.findWithBinding(openId, 1);
         TextResponse text = new TextResponse(map);
         switch (eventType) {
             case Event.CLICK:
-                ClickEvent clickEvent = new ClickEvent(map);
-                if (clickEvent.getEventKey().equals(UsingButton.SIGN)) {
-                    studentService.updateBinding(false, openId);
-                    text.setContent("解绑成功");
-                } else if (clickEvent.getEventKey().equals(UsingButton.HELP)) {
-                    text.setContent("帮助");
-                }
-                response = MessageUtil.toXml(text);
                 break;
             case Event.LOCATION:
-                LocationEvent locationEvent = new LocationEvent(map);
-                if (student != null) {
-                    Double longitude = locationEvent.getLongitude();
-                    Double latitude = locationEvent.getLatitude();
-                    studentService.updateLocation(longitude, latitude, openId);
-                }
                 break;
             case Event.LOCATION_SELECT:
                 break;
@@ -100,71 +78,15 @@ public class HandleRequestService {
             case Event.SCANCODE_PUSH:
                 break;
             case Event.SCANCODE_WAITMSG:
-                ScanCodeEvent code = new ScanCodeEvent(map);
-                String codeInfo = code.getScanResult();
-                JSONObject object = JSON.parseObject(codeInfo);
-                Integer courseId = object.getInteger("id");
-                Integer expire = object.getInteger("expire");
-                Long signTime = object.getLong("date");
-                if (student != null) {
-                    Integer studentId = student.getId();
-                    Relation relation = relationService.findByCourseIdAndStudentId(courseId, studentId);
-                    if (relation == null) {
-                        relation = new Relation();
-                        relation.setCourseId(courseId);
-                        relation.setStudentId(studentId);
-                        relationService.save(relation);
-                    }
-                    Date now = new Date();
-                    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-                    Date past = null;
-                    try {
-                        past = format.parse(format.format(now));
-                    } catch (ParseException e) {
-                        e.printStackTrace();
-                    }
-                    Sign sign = signService.findTodaySign(courseId, studentId, past);
-                    Double longitude = student.getLongitude();
-                    Double latitude = student.getLatitude();
-                    if (sign == null) {
-                        sign = new Sign();
-                        sign.setCourseId(courseId);
-                        sign.setStudentId(studentId);
-                        sign.setTime(now);
-                        sign.setLongitude(longitude);
-                        sign.setLatitude(latitude);
-                        if (signTime + expire < now.getTime()) {
-                            sign.setResult("签到");
-                            text.setContent("签到成功！");
-                        } else {
-                            sign.setResult("迟到");
-                            text.setContent("你已迟到！");
-                        }
-                        signService.save(sign);
-                    } else {
-                        if (sign.getLatitude() == null || sign.getLongitude() == null) {
-                            Long signId = sign.getId();
-                            if (signTime + expire < now.getTime()) {
-                                signService.updateSign(longitude, latitude, now, "签到", signId);
-                                text.setContent("签到成功！");
-                            } else {
-                                signService.updateSign(longitude, latitude, now, "迟到", signId);
-                                text.setContent("你已迟到！");
-                            }
-                        } else {
-                            text.setContent("你已经签到过了！");
-                        }
-                    }
-                } else {
-                    text.setContent("请先绑定学生信息！");
-                }
                 response = MessageUtil.toXml(text);
                 break;
             case Event.SUBSCRIBE:
-                text.setContent("欢迎关注趣课微信公主号！");
+                text.setContent("欢迎关注 车前半夏当归！");
                 response = MessageUtil.toXml(text);
                 break;
             case Event.UNSUBSCRIBE:
+                text.setContent("再见！");
+                response = MessageUtil.toXml(text);
                 break;
             case Event.VIEW:
                 break;
