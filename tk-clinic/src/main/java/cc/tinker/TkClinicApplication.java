09f49e1bf88ba72cc8e991f4098f9c517bea0abf
tinker <whoszus@yahoo.com>
2018-05-06 21:57:40 +0800

    提交新模块 --预约功能；

---
+++ tk-clinic/src/main/java/cc/tinker/TkClinicApplication.java
@@ -0,0 +1,15 @@
+package cc.tinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eagle
+ */
+@SpringBootApplication
+public class TkClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TkClinicApplication.class, args);
+	}
+}
