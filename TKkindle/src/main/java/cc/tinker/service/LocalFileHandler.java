74b9eab41f91abd60fccdc974a3d3a890b627db9
tinker <whoszus@yahoo.com>
2018-05-20 08:31:49 +0800

    新模块eureka

---
+++ TKkindle/src/main/java/cc/tinker/service/LocalFileHandler.java
@@ -0,0 +1,17 @@
+package cc.tinker.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Tinker
+ * 文件处理
+ */
+@Service
+public class LocalFileHandler {
+
+    public void  fileTableHandler(String  filePath){
+
+    }
+
+
+}
---
+++ tk-zuul/src/main/java/cc/tinker/ZuulApplication.java
@@ -0,0 +1,15 @@
+package cc.tinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Eagle
+ */
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+}
